d04b190140ff7d3f89d610bf9aa99806131f26fa
Ryan Robertson <ryguyrobertson@gmail.com>
2024-02-08 11:21:19 -0800

    add addProperty function to add new properties to PSQL DB

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -160,21 +160,6 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 
-// const getAllProperties1 = (options, limit = 10) => {
-//   return pool
-//     .query(`
-//       SELECT * FROM properties
-//         LIMIT $1;
-//       `,
-//       [limit])
-//     .then((result) => {
-//       return result.rows
-//     })
-//     .catch((err) => {
-//       console.log(err.message);
-//     });
-// };
-// Refactor refactor
 const getAllProperties = (options, limit = 10) => {
   const queryParams= [];
   // 
@@ -232,7 +217,6 @@ const getAllProperties = (options, limit = 10) => {
     ORDER BY cost_per_night
     LIMIT $${queryParams.length};
   `;
-  console.log(queryString, queryParams);
 
   return pool
     .query(queryString, queryParams)
@@ -249,12 +233,45 @@ const getAllProperties = (options, limit = 10) => {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
+// Refactor
+
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-};
+  return pool
+    .query(`
+      INSERT INTO properties (owner_id,
+        title,
+        description,
+        thumbnail_photo_url,
+        cover_photo_url,
+        cost_per_night,
+        street,
+        city,
+        province,
+        post_code,
+        country,
+        parking_spaces,
+        number_of_bathrooms,
+        number_of_bedrooms)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+      RETURNING *;
+    `,
+    [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms]
+    )
+    .then((result) => {
+        return result.rows
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+}
+
+// Old
+// const addProperty2 = function (property) {
+//   const propertyId = Object.keys(properties).length + 1;
+//   property.id = propertyId;
+//   properties[propertyId] = property;
+//   return Promise.resolve(property);
+// };
 
 module.exports = {
   getUserWithEmail,
